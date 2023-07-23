48daf330dae97a61003f3bde5cb43450e4f64181
Chris McHardy <chrismchardy1@gmail.com>
2023-07-23 13:22:34 +0200

    Init

---
+++ src/main/java/dev/chrismc/cinemaniaapi/CinemaniaApiApplication.java
@@ -0,0 +1,13 @@
+package dev.chrismc.cinemaniaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaniaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaniaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/chrismc/cinemaniaapi/CinemaniaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.chrismc.cinemaniaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaniaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
