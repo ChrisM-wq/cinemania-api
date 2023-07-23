22ed3a9eb719210e9c9edb0c2149727729cb1996
Chris McHardy <chrismchardy1@gmail.com>
2023-07-23 16:27:15 +0200

    Implement Cors config to allow local development

--- src/main/java/dev/chrismc/cinemaniaapi/CinemaniaApiApplication.java
+++ src/main/java/dev/chrismc/cinemaniaapi/CinemaniaApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CinemaniaApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CinemaniaApiApplication.class, args);
 	}
-
 }
